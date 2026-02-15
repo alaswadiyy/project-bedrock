a28cd93abeb9a7f5d1ccf4bc21ceeab39d5fa978
alaswadiyy <abdulmuizsharafadeen@gmail.com>
2026-02-15 05:27:28 +0100

    fix

--- terraform/envs/prod/backend.tf
+++ terraform/envs/prod/backend.tf
@@ -3,8 +3,8 @@ terraform {
     bucket         = "bedrock-assests-alt-soe-025-1329"
     key            = "terraform.tfstate"
     region         = "us-east-1"
-    #dynamodb_table = "project-bedrock-tfstate-lock"
-    use_lockfile = true
+    dynamodb_table = "project-bedrock-tfstate-lock"
+    #use_lockfile = true
     encrypt        = true
   }
 }
